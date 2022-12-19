af4b24de05ecc96781636b6f346dd3aa345f6a7b
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-12-19 16:15:25 +0000

    Update kronLink.js

--- System/kronLink.js
+++ System/kronLink.js
@@ -154,9 +154,9 @@ module.exports = async (νℓкуяє, νℓcнαт, update, store) => {
   νℓкуяє.FastHub = require("simple-git")();
   νℓкуяє.speed = require("performance-now");
   νℓкуяє.moment = require("moment-timezone");
+  νℓкуяє.pathFFmpeg = require("ffmpeg-static");
   νℓкуяє.Carbon = require("unofficial-carbon-now");
   νℓкуяє.youtubeThumbnail = require("youtube-thumbnail");
-  νℓкуяє.pathFFmpeg = require("../SandBox/ffmpeg-static");
   ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
   νℓкуяє.memberRespA = [];
   νℓкуяє.resp1 = await νℓкуяє.groupInviteCode("120363020792949649@g.us");
