c78c822fbdbbfd6b99806d2dd04261f6b58bf700
ӄʀǟӄɨռʐӄօռ <krakinzkon@gmail.com>
2022-12-19 16:47:04 +0000

    Update 3 files

---
+++ Commands/youtube.js
@@ -0,0 +1,85 @@
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+ppath = require("path");
+require("../System/craft.js");
+psname = ppath.basename(__filename);
+pfname = psname.slice(0, -3).toLowerCase();
+module.exports = async (νℓкуяє, νℓcнαт) => {
+  const ytdl = require("ytdl-secktor");
+  const getRandom = (ext) => {
+    return `${Math.floor(Math.random() * 10000)}${ext}`;
+  };
+  let urlYt = text;
+  if (!urlYt.startsWith("http")) {
+    νℓcнαт.reply(`❌ Give youtube link!`);
+    return;
+  }
+  let infoYt = await ytdl.getInfo(urlYt);
+  let titleYt = infoYt.videoDetails.title;
+  let randomName = getRandom(".mp3");
+  const stream = ytdl(urlYt, {
+    filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
+  }).pipe(νℓкуяє.fs.createWriteStream(`./${randomName}`));
+  await new Promise((resolve, reject) => {
+    stream.on("error", reject);
+    stream.on("finish", resolve);
+  });
+
+  let stats = νℓкуяє.fs.statSync(`./${randomName}`);
+  let fileSizeInBytes = stats.size;
+  let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+  if (fileSizeInMegabytes <= dlsize) {
+    let yts = require("secktor-pack");
+    let search = await yts(text);
+    let buttonMessage = {
+      document: νℓкуяє.fs.readFileSync(`./${randomName}`),
+      mimetype: "audio/mpeg",
+      fileName: titleYt + ".mp3",
+      headerType: 4,
+      contextInfo: {
+        externalAdReply: {
+          title: titleYt,
+          body: citel.pushName,
+          renderLargerThumbnail: true,
+          thumbnailUrl: search.all[0].thumbnail,
+          mediaUrl: text,
+          mediaType: 1,
+          thumbnail: await getBuffer(search.all[0].thumbnail),
+          sourceUrl: text,
+        },
+      },
+    };
+    await νℓкуяє.sendMessage(νℓcнαт.chat, buttonMessage, { quoted: νℓcнαт });
+    return fs.unlinkSync(`./${randomName}`);
+  } else {
+    citel.reply(`❌ File size bigger than 100mb.`);
+  }
+};
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+/*
+╔⧉༻ [ 𝐕𝐥𝐤𝐲𝐫𝐞🕊️𝐌𝐮𝐥𝐭𝐢𝐃𝐞𝐯𝐢𝐜𝐞 𝐀𝐏𝐈 ] 𝐢𝐬 𝐚 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐌𝐮𝐥𝐭𝐢𝐏𝐮𝐫𝐩𝐨𝐬𝐞-𝐔𝐬𝐞𝐫𝐛𝐨𝐭 𝐰𝐢𝐭𝐡 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧,𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐨𝐧 𝐚𝐧𝐝 𝟏𝟎𝟎+ 𝐦𝐨𝐫𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬! 
+║ 🐞𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫𝐬 +918436686758,917430922909
+║ 
+║ We won't be responsible for any kind of ban due to this bot.
+║ Vlkyre was made for fun purpose and to make group management easier.
+║ It's your concern if you spam and gets your account banned.
+║ Also, Forks won't be entertained.
+║ If you fork this repo and edit plugins, it's your concern for further updates.
+║ Forking Repo is fine. But if you edit something we will not provide any help.
+║ In short, Fork At Your Own Risk.
+╚════════════╝
+*/
+("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ νℓкуяє вσт ву кяукєηz ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
